966fff88221a7c44fa6463f374c7cdb2cfa17c45
Ninad Gawad <ninadgawad@gmail.com>
2022-01-17 14:42:37 -0500

    Create TreeCounter.scala

---
+++ TreeCounter.scala
@@ -0,0 +1,51 @@
+package org.ninad.spark.samples
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+/**
+* Usage: TreeCounter <tree_file_dataset>
+*/
+object TreeCounter {
+  def main(args: Array[String]) {
+    val spark = SparkSession
+      .builder
+      .appName("TreeCounter")
+      .getOrCreate()
+
+    if (args.length < 1) {
+      print("Usage: TreeCounter <tree_file_dataset>")
+      sys.exit(1)
+    }
+    // get the tree data set file name
+    val treeFile = args(0)
+    // read the file into a Spark DataFrame
+    val treeDF = spark.read.format("csv")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .load(mnmFile)
+    // display DataFrame
+    treeDF.show(5, false)
+
+    // aggregate count of all colors and groupBy state and color
+    val counttreeDF = treeDF.select("State", "Color", "Count")
+        .groupBy("State", "Color")
+        .sum("Count")
+        .orderBy(desc("sum(Count)"))
+
+    // show all the resulting aggregation for all the dates and colors
+    counttreeDF.show(60)
+    println(s"Total Rows = ${counttreeDF.count()}")
+    println()
+
+    // find the aggregate count for NY by filtering
+    val nyCountMnNDF = treeDF.select("*")
+      .where(col("State") === "NY")
+      .groupBy("State", "Color")
+      .sum("Count")
+      .orderBy(desc("sum(Count)"))
+
+    // show the result 
+    nyCountMnNDF.show(10)
+  }
+}
