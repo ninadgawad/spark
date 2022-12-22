b811d811a57d0d9fdc63e5ccd9a19dd8aea7888f
Ninad Gawad <ninadgawad@gmail.com>
2022-12-22 11:28:38 -0500

    Update SparkFunctions.java

--- SparkFunctions.java
+++ SparkFunctions.java
@@ -5,7 +5,27 @@ import org.apache.spark.sql.Dataset;
 public class SparkFunctions {
 
   public static void main(String[] arguments){
-
+      
+      // Create a session
+      SparkSession sparkSession = SparkSession.builder().appName("SparkFunctions").master("local").getOrCreate();
+     
+     // Read the Data
+     DataSet<Row> users =  sparkSession.read().option("header","true").csv("users.csv");
+     
+     // See sample data 
+     users.show();
+     
+     // Cache and create a View
+     users.createOrReplaceTempView("users")
+      
+     // Use the Rank Function 
+     DataSet<Row> usersRank = sparkSession.sql("select amount, rank() over(order by amount desc) as rank from users");
+                     
+    // See sample data 
+     usersRank.show();
+   
+       
+    
     
   }
 
