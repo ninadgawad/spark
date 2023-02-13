bd0887414d8c6b41bc86fed049c7835f20298ad1
Ninad Gawad <ninadgawad@gmail.com>
2023-02-12 23:18:29 -0500

    Update SparkDrools.scala

--- SparkDrools.scala
+++ SparkDrools.scala
@@ -6,3 +6,21 @@ import org.kie.api.builder.KieBuilder;
 import org.kie.api.builder.KieFileSystem;
 import org.kie.api.builder.KieModule;
 import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+
+public class SparkDrools {
+
+    public static void main(String[] args) {
+      
+    }
+  
+  
+  
+}
