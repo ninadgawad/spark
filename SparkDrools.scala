99bf990dfc76872f01e99cc4365bd11b5ea8b76e
Ninad Gawad <ninadgawad@gmail.com>
2023-02-12 23:19:32 -0500

    Update SparkDrools.scala

--- SparkDrools.scala
+++ SparkDrools.scala
@@ -18,9 +18,44 @@ import org.apache.spark.api.java.function.Function;
 public class SparkDrools {
 
     public static void main(String[] args) {
-      
+
+        // Initialize the Spark context
+        JavaSparkContext sc = new JavaSparkContext("local[2]", "Drools Spark Example");
+
+        // Load the data into a Spark RDD
+        List<Integer> data = new ArrayList<>();
+        data.add(1);
+        data.add(2);
+        data.add(3);
+        JavaRDD<Integer> rdd = sc.parallelize(data);
+
+        // Initialize the Drools KieBase
+        KieServices ks = KieServices.Factory.get();
+        KieFileSystem kfs = ks.newKieFileSystem();
+        kfs.write("src/main/resources/rules.drl", "package drools.spark.example\n\nrule \"Hello World\"\n\twhen\n\t\t$i : Integer(intValue > 1)\n\tthen\n\t\tSystem.out.println(\"Hello World: \" + $i);\nend");
+        KieBuilder kieBuilder = ks.newKieBuilder(kfs).buildAll();
+        KieModule kieModule = kieBuilder.getKieModule();
+        KieContainer kieContainer = ks.newKieContainer(kieModule.getReleaseId());
+        KieBase kieBase = kieContainer.getKieBase();
+
+        // Apply the Drools rules to the RDD
+        JavaRDD<Integer> result = rdd.map(new Function<Integer, Integer>() {
+            public Integer call(Integer i) {
+                KieSession kieSession = kieBase.newKieSession();
+                kieSession.insert(i);
+                kieSession.fireAllRules();
+                kieSession.dispose();
+                return i;
+            }
+        });
+
+        // Collect the results and print them
+        List<Integer> output = result.collect();
+        for (Integer i : output) {
+            System.out.println(i);
+        }
+
+        // Stop the Spark context
+        sc.stop();
     }
-  
-  
-  
 }
