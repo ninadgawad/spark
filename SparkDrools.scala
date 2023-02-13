61c602ab3b4c87c94b5136eef074a281a58e7c18
Ninad Gawad <ninadgawad@gmail.com>
2023-02-12 23:18:02 -0500

    Create SparkDrools.scala

---
+++ SparkDrools.scala
@@ -0,0 +1,8 @@
+import org.drools.core.ClassObjectFilter;
+import org.drools.core.impl.KnowledgeBaseImpl;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.KieModule;
+import org.kie.api.runtime.KieContainer;
