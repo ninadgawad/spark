e0b869adbd2b77da488f3e88a164450f66555f23
Ninad Gawad <ninadgawad@gmail.com>
2023-02-26 19:28:21 -0500

    Update App.scala

--- project/spark-scala-app/App.scala
+++ project/spark-scala-app/App.scala
@@ -2,14 +2,20 @@ package org.ninad
 
 import scala.math.random
 import org.apache.spark.sql.SparkSession
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
 object App {
 
   def main(args: Array[String]): Unit = {
     println("Hello Spark")
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
     val spark = SparkSession
       .builder
+      .master("local[1]")
       .appName("Spark Pi")
       .getOrCreate()
+
     val slices = if (args.length > 0) args(0).toInt else 2
     val n = math.min(100000L * slices, Int.MaxValue).toInt // avoid overflow
     val count = spark.sparkContext.parallelize(1 until n, slices).map { i =>
